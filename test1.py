d465f1915a005ba1380cd7b1aea974ada253af83
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-11-15 21:11:39 +0530

    Leet code

---
+++ test1.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        
+        return candidate
+nums = [2, 2, 1, 1, 1, 2, 2]
+sol = Solution()
+print(sol.majorityElement(nums))
